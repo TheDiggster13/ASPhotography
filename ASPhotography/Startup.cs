d06c6e0719cc674466e084c563422a091fc04fc4
Joshua Berry <j.berry@parcel2go.com>
2016-06-10 14:19:09 +0100

    First build of the site

---
+++ ASPhotography/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPhotography.Startup))]
+namespace ASPhotography
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
